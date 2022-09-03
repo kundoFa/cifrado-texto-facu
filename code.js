45da09ea9530cc036925712aaf16d6e2acfa7fc1
facundo <ferreyranego1@gmail.comm>
2022-09-02 23:35:40 -0300

    criptador terminado

--- code.js
+++ code.js
@@ -1,37 +1,41 @@
 const formAction = document.getElementById("form_control")
 const visorRes = document.getElementById("resHTML")
-
-const criptadorTexto = () =>{
-
-
-
-
-	let txx = "abcdefghijkmñnopqvxwyz"
-	let textRess = ""
-	let txToMod = "hola mundo"
-	for(let i = 0;i<=txx.length -1;i++){
-		const e = txToMod[i]
-		const eMod = txx[i+2]
-		
-		if(e == undefined) continue
-		textRess += e
-
-
-		/* switch(i){
-			case "a" : textRess =+ "" 
-
-				
-		} */
+/* const valTextArea = document.getElementById("textterArea").value*/
+function encriptar (texto){
+	var textoCifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
+	visorRes.innerHTML = textoCifrado
+}
+function desencriptar (texto){
+	var textoCifrado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
+	visorRes.innerText = textoCifrado;
+}
+const criptadorTexto = (txx,val) =>{
+	if(val == "en"){
+		encriptar(txx)
 	}
-	console.log(textRess)
+	else if (val == "des"){
+		desencriptar(txx)
+	}
+	
+}
+const veri = (en,des)=>{
+	if(en.checked){
+		return "en"
+	}
+	else if (des.checked){
+		return "des"
+	}
 }
 
-
 formAction.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    const valTextArea = e.target[0].value;
+	e.preventDefault();
+	const valTextArea = e.target[0].value;
+
+	
+
+	criptadorTexto(valTextArea,veri(e.target[2],e.target[1]))
     
     
 
 })
-criptadorTexto()
+/* criptadorTexto() */
